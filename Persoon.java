0b8273aa6ad25b5b4dd9a11bc61abf7fad42af36
RobinFrancq <32958630+RobinFrancq@users.noreply.github.com>
2017-10-20 15:06:53 +0200

    Create Persoon.java

---
+++ Persoon.java
@@ -0,0 +1,28 @@
+public class Dier {
+	
+	private String naam;
+
+	public Dier() {}
+	
+	public Dier(String naam) {
+		this.naam = naam;
+	}
+	
+	public String getNaam() {
+		return naam;
+	}
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+	
+	public String getType() {
+		String type;
+		type = this.getClass().getSimpleName();
+		return type;
+	}
+	
+	public String toString() {
+		String type = this.getType();
+		return naam + " " + type;
+	}
+}
